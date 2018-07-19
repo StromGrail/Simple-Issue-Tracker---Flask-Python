b3c619cd46fcca2ddce05e9f057e7c2598087c63
StormGrail <mohitatul0803@gmail.com>
2018-07-19 08:39:39 +0530

    first commit

--- issuetracker/routes.py
+++ issuetracker/routes.py
@@ -3,62 +3,71 @@ from issuetracker import app, db
 from issuetracker.forms import RegistrationForm, LoginForm, PostIssueForm
 from issuetracker.models import User, Post
 
-userdetails = ''
+userdetails = None
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    global userdetails
     user = User.query.all()
-    userinfo=None
+    userdetails
     for u in user :
         if u.email == userdetails:
-            userinfo=u
-    return render_template('home.html', user=user,userinfo=userinfo)
+            userdetails=u
+    return render_template('home.html', user=user,userdetails=userdetails)
 
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    global userdetails
     form = RegistrationForm()
     if form.validate_on_submit():
         flash("Account created for {}!".format(form.username.data), 'success')
-        global userdetails
         userdetails = form.username.data
         user= User(FirstName=form.FirstName.data,LastName= form.LastName.data,username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form,userdetails=userdetails)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    global userdetails
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.all()
         for u in user :
             if form.email.data == u.email:
                 if form.password.data == u.password:
-                    global userdetails
                     userdetails = form.email.data
                     print(userdetails)
                     flash('You have been logged in!', 'success')
                     return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title='Login', form=form,userdetails=userdetails)
+
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    global userdetails
+    userdetails=None
+    return redirect(url_for('home'))
+
 
 @app.route("/issue", methods=['GET', 'POST'])
 def postissue():
+    global userdetails
     form = PostIssueForm(request.form)
-    if request.method == "POST" :
-        global userdetails
+    if request.method == "POST" and userdetails!=None:
         user = User.query.all()
         flag,username ,checkname = False,'',form.AssignedTo.data 
         print(checkname)
         for u in user:
-            if u.email== userdetails:
+            if u.email== userdetails.email:
                 username=u.username
 
         for u in user:
@@ -77,6 +86,8 @@ def postissue():
             flash('Something went wrong .... Please try again.', 'danger')   
             if flag==False:
                 flash('Wrong Assignment', 'danger')   
-                     
+    elif userdetails==None:
+        flash('Login to proceed', 'danger')
+        return redirect(url_for('login'))  
 
-    return render_template('issue.html',title= 'New Issue', username=userdetails , form= form)
+    return render_template('issue.html',title= 'New Issue', userdetails=userdetails , form= form)
