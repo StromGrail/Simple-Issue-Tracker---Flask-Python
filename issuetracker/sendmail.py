5e5ef8eb7b03a74df6735cf38e8a9debc3cb3776
Mohit Atul <mohitatul0803@gmail.com>
2018-07-19 07:42:02 +0530

    Delete sendmail.py

--- issuetracker/sendmail.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-content="Example email"
-mail = smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login('mohitatul0803@gmail.com',password)
-mail.sendmail('mohitatul0803','mohitatul0803@gmail.com',content)
-mail.close()
